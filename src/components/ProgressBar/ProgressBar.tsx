e2ba6dd2678f052b898e57067cfc27c922ff61b3
jinhwansuh <nick02190219@gmail.com>
2022-11-05 20:57:38 +0900

    feat: prop을 받을수 있도록 변경

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -3,13 +3,28 @@ import styled from 'styled-components';
 
 export interface ProgressBarProps {
   top?: number;
+  right?: number;
   bottom?: number;
+  left?: number;
   height?: number;
   color?: string;
   duration: number;
+  width?: number | string;
+  position?: string;
 }
 
-function ProgressBar({ duration, ...rest }: ProgressBarProps) {
+function ProgressBar({
+  duration,
+  top = 0,
+  right = 0,
+  bottom = 0,
+  left = 0,
+  height = 20,
+  color = 'red',
+  width = '100%',
+  position = 'absolute',
+  ...rest
+}: ProgressBarProps) {
   const [show, setShow] = useState(true);
 
   useEffect(() => {
@@ -18,34 +33,46 @@ function ProgressBar({ duration, ...rest }: ProgressBarProps) {
     }, duration * 1000);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [duration]);
 
   return (
-    <Container style={{ opacity: show ? 1 : 0 }}>
+    <Container>
       <StyledProgressBar
-        style={{ animationDuration: `${duration}s` }}
+        top={top}
+        right={right}
+        bottom={bottom}
+        left={left}
+        height={height}
+        color={color}
+        width={width}
+        position={position}
+        duration={duration}
+        style={{
+          opacity: show ? 1 : 0,
+        }}
         {...rest}
       />
     </Container>
   );
 }
 
-const Container = styled.div<Pick<ProgressBarProps, 'height'>>`
+const Container = styled.div`
   position: relative;
-  height: ${(props) => props.height || '20px'};
-  width: 100%;
 `;
 
-const StyledProgressBar = styled.div<Pick<ProgressBarProps, 'color'>>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: ${(props) => props.color || 'red'};
+const StyledProgressBar = styled.div<ProgressBarProps>`
+  position: ${(props) => props.position};
+  top: ${(props) => props.top};
+  right: ${(props) => props.right};
+  bottom: ${(props) => props.bottom};
+  left: ${(props) => props.left};
+  width: ${(props) => `${props.width}px`};
+  height: ${(props) => `${props.height}px`};
+  background-color: ${(props) => props.color};
   animation-name: progress;
   animation-fill-mode: forwards;
   animation-timing-function: linear;
+  animation-duration: ${(props) => `${props.duration}s`};
 
   @keyframes progress {
     0% {
